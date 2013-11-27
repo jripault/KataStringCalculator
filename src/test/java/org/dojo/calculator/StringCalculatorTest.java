14ac2efb1740d0d77ebef3e8995b61ef9886340e
Charles Bouttaz <charles.bouttaz@atos.net>
2013-11-27 10:55:43 +0100

    add sample test

--- src/test/java/org/dojo/calculator/StringCalculatorTest.java
+++ src/test/java/org/dojo/calculator/StringCalculatorTest.java
@@ -1,5 +1,21 @@
 package org.dojo.calculator;
 
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
 public class StringCalculatorTest {
+
+    @Test
+    public void should_do_something(){
+        //Given
+        int i = 0;
+
+        //When
+        i++;
+
+        //Then
+        assertTrue(i == 1);
+    }
 
 }
