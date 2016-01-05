99ef83c73872e5d45a2e40c82574b5bc638f5317
jripault <tluapir@gmail.com>
2016-01-05 22:05:11 +0100

    End of the Kata

--- src/main/java/org/dojo/calculator/StringCalculator.java
+++ src/main/java/org/dojo/calculator/StringCalculator.java
@@ -1,16 +1,36 @@
 package org.dojo.calculator;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class StringCalculator {
 
 
     public int add(String s) {
+        String delimiter = ",|\n";
+        boolean hasNegativeNumbers = false;
+        List<Integer> negativeNumbers = new ArrayList<Integer>();
         if("".equals(s)){
             return 0;
         }
-        String[] numbers = s.split(",|\n");
+        if(s.startsWith("//")){
+            int indexOf = s.indexOf("\\n");
+            delimiter = s.substring(2, indexOf);
+            s = s.substring(indexOf + 2);
+        }
+        String[] numbers = s.split(delimiter);
         int sum = 0;
         for (int i = 0; i < numbers.length; i++) {
-            sum += Integer.parseInt(numbers[i]);
+            int number = Integer.parseInt(numbers[i]);
+            if(number < 0){
+                hasNegativeNumbers = true;
+                negativeNumbers.add(number);
+            }
+            sum += number;
+        }
+
+        if(hasNegativeNumbers){
+            throw new RuntimeException("Negatives not allowed:" + negativeNumbers.toString());
         }
         return sum;
     }
--- src/test/java/org/dojo/calculator/StringCalculatorTest.java
+++ src/test/java/org/dojo/calculator/StringCalculatorTest.java
@@ -4,6 +4,7 @@ import org.junit.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
 
 public class StringCalculatorTest {
 
@@ -56,5 +57,29 @@ public class StringCalculatorTest {
         int result = calculator.add("1\n2,3");
         // Then
         assertThat(result).isEqualTo(6);
+    }
+    @Test
+    public void shouldSupportDelimiter() throws Exception {
+
+        // Given
+        StringCalculator calculator = new StringCalculator();
+        // When
+        int result = calculator.add("//;\\n1;2");
+        // Then
+        assertThat(result).isEqualTo(3);
+    }
+    @Test
+    public void shouldThrowExceptionWithNegativeNumbers() throws Exception {
+
+        // Given
+        StringCalculator calculator = new StringCalculator();
+        // When
+        try {
+            int result = calculator.add("-1,2,-3");
+            fail("Exception expected ");
+        } catch (Exception e) {
+            // Then
+            assertThat(e).hasMessage("Negatives not allowed:[-1, -3]");
+        }
     }
 }
