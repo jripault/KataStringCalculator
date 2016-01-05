61e45219eb43d06b44c0550367bc8c7e6dea1bdd
jripault <tluapir@gmail.com>
2016-01-05 21:45:51 +0100

    WIP StringCalculator

--- src/main/java/org/dojo/calculator/StringCalculator.java
+++ src/main/java/org/dojo/calculator/StringCalculator.java
@@ -3,4 +3,15 @@ package org.dojo.calculator;
 public class StringCalculator {
 
 
+    public int add(String s) {
+        if("".equals(s)){
+            return 0;
+        }
+        String[] numbers = s.split(",|\n");
+        int sum = 0;
+        for (int i = 0; i < numbers.length; i++) {
+            sum += Integer.parseInt(numbers[i]);
+        }
+        return sum;
+    }
 }
--- src/test/java/org/dojo/calculator/StringCalculatorTest.java
+++ src/test/java/org/dojo/calculator/StringCalculatorTest.java
@@ -2,20 +2,59 @@ package org.dojo.calculator;
 
 import org.junit.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.Assert.assertTrue;
 
 public class StringCalculatorTest {
 
     @Test
-    public void should_do_something(){
-        //Given
-        int i = 0;
+    public void shouldReturnZeroWhenEmpty() throws Exception {
 
-        //When
-        i++;
-
-        //Then
-        assertTrue(i == 1);
+        // Given
+        StringCalculator calculator = new StringCalculator();
+        // When
+        int result = calculator.add("");
+        // Then
+        assertThat(result).isEqualTo(0);
     }
+    @Test
+    public void shouldReturnOne() throws Exception {
 
+        // Given
+        StringCalculator calculator = new StringCalculator();
+        // When
+        int result = calculator.add("1");
+        // Then
+        assertThat(result).isEqualTo(1);
+    }
+    @Test
+    public void shouldReturnThree() throws Exception {
+
+        // Given
+        StringCalculator calculator = new StringCalculator();
+        // When
+        int result = calculator.add("1,2");
+        // Then
+        assertThat(result).isEqualTo(3);
+    }
+    @Test
+    public void shouldSumMultipleNumbers() throws Exception {
+
+        // Given
+        StringCalculator calculator = new StringCalculator();
+        // When
+        int result = calculator.add("1,2,4,1");
+        // Then
+        assertThat(result).isEqualTo(8);
+    }
+    @Test
+    public void shouldHandleLineBreaks() throws Exception {
+
+        // Given
+        StringCalculator calculator = new StringCalculator();
+        // When
+        int result = calculator.add("1\n2,3");
+        // Then
+        assertThat(result).isEqualTo(6);
+    }
 }
